a6a2924a45b0cc5c3ec5b8b2ece962c247371dec
Uesley Carossi <uesley.carossi@gmail.com>
2020-09-19 14:50:39 -0300

    1581

---
+++ src/uri/U_1581.java
@@ -0,0 +1,35 @@
+package uri;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class U_1581 {
+ 
+    public static void main(String[] args) throws IOException {
+    	Scanner input = new Scanner(System.in);
+    	
+    	final int qtdGroup = input.nextInt();
+    	for (int i = 0; i < qtdGroup; i++) {
+    		
+    		int qtdPeople = input.nextInt();
+    		String firstLanguage = null;
+    		String language = null;
+    		
+    		for (int j = 0; j < qtdPeople; j++) {
+    			
+				if (j == 0) {
+					firstLanguage = input.next();
+					language = firstLanguage;
+				} else if (!firstLanguage.equals(input.next())) {
+					language = "ingles";
+				}
+    			
+			}
+    		
+    		System.out.println(language);
+		}
+ 
+    	input.close();
+    }
+ 
+}
