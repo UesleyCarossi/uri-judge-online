b7ca70ee383b5fc691d8f2d8135e9919dc47d81a
Uesley Carossi <uesley.carossi@gmail.com>
2020-09-26 14:21:40 -0300

    1630

--- src/uri/U_1630.java
+++ src/uri/U_1630.java
@@ -4,13 +4,30 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class U_1630 {
- 
-    public static void main(String[] args) throws IOException {
-    	Scanner input = new Scanner(System.in);
-    	
-    	
- 
-    	input.close();
-    }
- 
+
+	public static void main(String[] args) throws IOException {
+		Scanner input = new Scanner(System.in);
+		
+		int x, y;
+		
+		while(input.hasNext()) {
+			x = input.nextInt();
+			y = input.nextInt();
+			
+			int mdc = mdc(x, y);
+			
+			System.out.println(((x/mdc)+(y/mdc))*2);
+		}
+		
+		input.close();
+	}
+
+	public static int mdc(int x, int y) {
+		if ((x % y == 0)) {
+			return y;
+		} else {
+			return mdc(y, (x % y));
+		}
+	}
+
 }
